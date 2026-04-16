4f43cc605f9f04734cbd503bc8e3b0c10751bc6d
Mohammed47107 <benjamin.alegre@alumnos.ucentral.cl>
2026-04-15 22:58:25 -0400

    se crea el ciclo de ingreso de datos, reservando memoria. al final se libera la memoria a traves de otro ciclo

--- labs/lab3/struct.c
+++ labs/lab3/struct.c
@@ -8,6 +8,24 @@ typedef struct Producto {
 } Producto;
 
 int main(){
+    int cantidadProductos = 3;
+    Producto* productos = (Producto*) malloc(cantidadProductos * sizeof(Producto));
+
+    for (int i = 0; i < cantidadProductos; i++) {
+        printf("producto: %d\n", i+1);
+        productos[i].nombre = (char*) malloc(21 * sizeof(char)); // no necesita & porque nombre ya es un puntero
+        printf("nombre: ");
+        scanf("%s", productos[i].nombre);
+        printf("precio: ");
+        scanf("%f", &productos[i].precio);
+        printf("cantidad: ");
+        scanf("%d", &productos[i].cantidad);
+    }
+
+    for (int i = 0; i < cantidadProductos; i++) {
+        free(productos[i].nombre); // Liberamos cada nombre reservado individualmente
+    }
+    free(productos);
 
     return 0;
 }
